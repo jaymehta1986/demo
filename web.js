27accb7a732e5a8945b89663d55de9c78bec9cb2
Jay Mehta <jaymehta1986@ymail.com>
2014-08-10 15:50:23 -0700

    add morgan and update web.js to run using node

--- web.js
+++ web.js
@@ -1,15 +1,10 @@
 // web.js
+var gzippo = require('gzippo');
 var express = require("express");
 var logfmt = require("logfmt");
+var morgan = require('morgan');
 var app = express();
 
-app.use(logfmt.requestLogger());
-
-app.get('/', function(req, res) {
-  res.send('Hello World!');
-});
-
-var port = Number(process.env.PORT || 5000);
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
+app.use(morgan('dev'));
+app.use(gzippo.staticGzip("" + __dirname + "/dist"));
+app.listen(process.env.PORT || 5000);
